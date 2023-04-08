f0fbcd855eb2f5df169805d5376d2c07594ca458
diegooviedo3004 <diegoenriqueoviedo2004@gmail.com>
2023-04-07 23:58:53 -0600

    Unicafe 1.10

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -2,59 +2,55 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { useState } from 'react'
 
-const Header = () => <h1>Give feedback</h1>
+const Header = () => <h1>Give feedback</h1>;
+
+const StaticsLine = ({value, text}) => <>{text} {value}<br/></>
 
 const Statistics = ({ good, neutral, bad }) => {
-
-  let average = 0
-  const totalVotes = good + neutral + bad
-
-
-  if (totalVotes > 0) {
-    console.log(good * 1 + bad * -1 / totalVotes)
-    average = ((good * 1) + (neutral * 0) + (bad * -1)) / totalVotes
-  }
+  const totalVotes = good + neutral + bad;
+  const average = totalVotes > 0 ? (good * 1 + bad * -1) / totalVotes : 0;
+  const positivePercentage = totalVotes > 0 ? (good / totalVotes) * 100 : 0;
 
   return (
     <>
       <h1>Statistics</h1>
       {totalVotes > 0 ? (
         <>
-        good {good} <br />
-        neutral {neutral}<br />
-        bad {bad}<br />
-        all {totalVotes} <br />
-        average {average}<br />
-        positive {(good / totalVotes) * 100}%
+          <StaticsLine text={"good"} value={good}/>
+          <StaticsLine text={"neutral"} value={neutral}/>
+          <StaticsLine text={"bad"} value={bad}/>
+          <StaticsLine text={"all"} value={totalVotes}/>
+          <StaticsLine text={"average"} value={average}/>
+          <StaticsLine text={"positive"} value={positivePercentage}/>
         </>
       ) : (
         <>No feedback given</>
       )}
-      
     </>
-  )
-}
+  );
+};
 
-const Button = (props) => <button onClick={props.handleClick}>{props.text}</button>
+const Button = ({ handleClick, text }) => (
+  <button onClick={handleClick}>{text}</button>
+);
 
 const App = () => {
-  const [feedback, setFeedback] = useState({
-    good: 0,
-    neutral: 0,
-    bad: 0
-  });
+  const [feedback, setFeedback] = useState({ good: 0, neutral: 0, bad: 0 });
 
-  const sumGood = () => setFeedback({ ...feedback, good: feedback.good + 1 });
-  const sumNeutral = () => setFeedback({ ...feedback, neutral: feedback.neutral + 1 });
-  const sumBad = () => setFeedback({ ...feedback, bad: feedback.bad + 1 });
+  const handleGoodClick = () =>
+    setFeedback({ ...feedback, good: feedback.good + 1 });
+  const handleNeutralClick = () =>
+    setFeedback({ ...feedback, neutral: feedback.neutral + 1 });
+  const handleBadClick = () =>
+    setFeedback({ ...feedback, bad: feedback.bad + 1 });
 
   return (
     <>
       <Header />
-      <Button text={"good"} handleClick={sumGood} />
-      <Button text={"neutral"} handleClick={sumNeutral} />
-      <Button text={"bad"} handleClick={sumBad} />
-      <Statistics good={feedback.good} bad={feedback.bad} neutral={feedback.neutral} />
+      <Button handleClick={handleGoodClick} text={"good"} />
+      <Button handleClick={handleNeutralClick} text={"neutral"} />
+      <Button handleClick={handleBadClick} text={"bad"} />
+      <Statistics good={feedback.good} neutral={feedback.neutral} bad={feedback.bad} />
     </>
   );
 };
